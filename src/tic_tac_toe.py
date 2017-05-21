69d24bbc30e7f07a3e3a3143dcf550582fb818c0
trex22 <contact@jasonchalom.com>
2017-05-21 19:40:11 +0200

    please

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -60,23 +60,26 @@ def checkWin(world, player):
 		for j in range(size):
 			if world[i][j] == player:
 				rowcount = rowcount+1
-			if world[j][i] == player:
-				colcount = colcount+1
-
 		rowcount = 0
+
+	for i in range(size):
+		for j in range(size):
+			if world[j][i] == player:
+				rowcount = rowcount+1
 		colcount = 0
 
+	for i in range(size):
 		if world[i][i] == player:
 			diag1count = diag1count+1
 		if world[i][size-1-i] == player:
 			diag2count = diag2count+1
 
-		if rowcount == size:
-			return True
-		if colcount == size:
-			return True
-		if diag1count == size or diag2count == size:
-			return True
+	if rowcount == size:
+		return True
+	if colcount == size:
+		return True
+	if diag1count == size or diag2count == size:
+		return True
 
 	return False
 
@@ -110,7 +113,7 @@ def rndVsRnd(size):
 
 	movesLeft = numberMovesLeft(world)
 	hasWon = False
-	while(movesLeft > 0) and not hasWon:
+	while(movesLeft > 0) and (hasWon == False):
 		# player 1
 		if (movesLeft > 0):
 			world = rndMove(world, 1.0)
@@ -119,13 +122,13 @@ def rndVsRnd(size):
 		printWorld(world);
 
 		# player 2
-		if (movesLeft > 0):
-			world = rndMove(world, -1);
-		hasWon = checkWin(world, -1)
-		# repeat till end - check who won
+		# if (movesLeft > 0):
+		# 	world = rndMove(world, -1);
+		# hasWon = checkWin(world, -1)
+		# # repeat till end - check who won
 
-		if (movesLeft > 0):
-			movesLeft = numberMovesLeft(world)
+		# if (movesLeft > 0):
+		# 	movesLeft = numberMovesLeft(world)
 		print("Moves Left: %d\n"%(movesLeft))
 
 rndVsRnd(3)
